ba5423a5dda7b0681df492d66bccc4ae60f6272d
Okonkwo Maryann Chidimma <you@example.com>
2020-10-17 07:46:33 -0700

    added user login

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const user = require('../models/user');
 
@@ -41,6 +42,37 @@ router.post('/signup', (req, res, next)=>{
     
     
 });
+//check if a certain user is in the database, then creat a token 
+router.post('/login', (req, res, next)=>{
+    user.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+     if(!user.length >1){
+         return res.status(401).json({message : "Authentication field"})
+     }
+     bcrypt.compare(req.body.password, user[0].password, (err, result) =>{
+         if(err){
+            return res.status(401).json({message : "Authentication field"})   
+         }
+         if(result){
+             token = jwt.sign({
+                email: user[0].email, 
+                user_Id: user[0]._Id
+                },
+                process.env.JWT_KEY,
+               {expiresIn : '1h'}
+             )
+            return res.status(200).json({message : "Authentication successful", tokens: token })  
+         }
+         return res.status(401).json({message : "Authentication field"})
+        })
+    
+    })
+    .catch(err=>{ 
+        res.status(500).json({error: err});
+    })
+
+})
 
 router.delete('/:userId', (req, res, next)=>{
    user.remove({_id: req.params.userId})
