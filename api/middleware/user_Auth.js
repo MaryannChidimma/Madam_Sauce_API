a31192f3322cf1c678c22beb1b767d09ff5609a6
Okonkwo Maryann Chidimma <you@example.com>
2020-10-18 01:55:26 -0700

    created controller for order

--- api/middleware/user_Auth.js
+++ api/middleware/user_Auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next ) =>{
     try{
         const token = req.headers.authorization.split(" ")[1];
-       // console.log(token);
+        console.log(token);
         const decoder = jwt.verify(token, process.env.JWT_KEY)
         req.userData = decoder;
         next();
