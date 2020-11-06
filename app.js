323a8051c36cc2b2ce9c7f53845c4206097b47a7
Okonkwo Maryann Chidimma <you@example.com>
2020-11-06 11:18:35 +0100

    prod env

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const config = require('config');
 
+if(app.get('env')==='production'){
+    mongoose.connect( process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("logged to database"))
+    .catch(err => console.error('could not connect to mongo db....', err))
+}
+if(app.get('env')==='development'){
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("logged to database"))
     .catch(err => console.error('could not connect to mongo db....', err))
+}
 
 const menuRoute = require('./api/routes/menu');
 const orderRoute = require('./api/routes/order');
-const userRoute = require('./api/routes/user');
+const adminRoute = require('./api/routes/admin');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -32,19 +40,21 @@ app.use(morgan('dev'));
 
 app.use('/menu', menuRoute);
 app.use('/order', orderRoute);
-app.use('/user', userRoute);
+app.use('/admin', adminRoute);
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
 });
-
-app.use((error, req, res, callack) => {
-
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
-        error: { message: error.message }
+        message: error.message,
+        success: false
     });
 });
+
+
+
 module.exports = app;
